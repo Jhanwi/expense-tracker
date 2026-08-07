8675537db458ee1ca47c37ccd2b6deba731fb28f
Jhanwi Kumari <jhanwitiwary@gmail.com>
2026-08-07 11:11:16 +0530

    Change database URL to expense-tracker.db
    Updated database URL to use 'expense-tracker.db'.

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./smart_spend.db"
+DATABASE_URL = "sqlite:///./expense-tracker.db"
 
 engine = create_engine(
     DATABASE_URL, 
